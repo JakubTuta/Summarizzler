72e324d00fc17e25e58b640254e5bf6fc1fe366a
JakubTuta <jakubtutka02@gmail.com>
2025-02-18 20:32:38 +0100

    fixed docker

--- frontend/stores/api.ts
+++ frontend/stores/api.ts
@@ -4,7 +4,8 @@ import axios, { AxiosError } from 'axios'
 import { ACCESS_TOKEN } from '~/constants/localStorage'
 
 export const useApiStore = defineStore('api', () => {
-  const baseURL = 'http://localhost:8000'
+  // const baseURL = 'http://localhost:8000'
+  const baseURL = 'https://server-783752631570.europe-central2.run.app'
 
   const defaultHeaders = {
     'Content-Type': 'application/json',
